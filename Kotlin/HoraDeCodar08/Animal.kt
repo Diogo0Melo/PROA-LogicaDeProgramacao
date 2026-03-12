c1cc1907b0fd50fb453bbcd99622267ec57fd95e
DIOGO EVANILSON DA COSTA MELO <diogo.ecmelo@senacsp.edu.br>
2026-03-12 12:49:27 -0300

    Iniciando atividade VirtualPet

---
+++ Kotlin/HoraDeCodar08/Animal.kt
@@ -0,0 +1,8 @@
+package HoraDeCodar08
+
+abstract class Animal(val nome:String, var apelido:String ) {
+    var idade:Int = 0
+    var nivelFome: Int = 50
+    var nivelFelicidade: Int = 50
+    
+}
