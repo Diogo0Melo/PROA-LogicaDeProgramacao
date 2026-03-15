72fe813e84dddd7923065e32d0201c861c24e8e9
Diogo Melo <140855720+Diogo0Melo@users.noreply.github.com>
2026-03-14 23:07:15 -0300

    remove Exercício em desenvolvimento

--- Kotlin/HoraDeCodar08/Animal.kt
+++
@@ -1,8 +0,0 @@
-package HoraDeCodar08
-
-abstract class Animal(val nome:String, var apelido:String ) {
-    var idade:Int = 0
-    var nivelFome: Int = 50
-    var nivelFelicidade: Int = 50
-    
-}
